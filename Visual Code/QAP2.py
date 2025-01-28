61fb8f8901ad8b4a0fe2d7efe8e1cd980abe1eb2
KassFequet <kassaundra.fequet@keyin.com>
2025-01-28 12:39:55 -0330

    QAP2Python

---
+++ Visual Code/QAP2.py
@@ -0,0 +1,48 @@
+# Description: QAP #2 - St. Johns Marina & Yacht Club
+# Author: Kassaundra Fequet
+# Date(s): January 27/24 - 
+ 
+ 
+# Define required libraries.
+ 
+ 
+ 
+# Define program constants.
+EVEN_SITE_RATE = 80.00 
+ODD_SITE_RATE = 120.00 
+ALT_MEM_RATE = 5.00 
+CLEAN_RATE = 50.00 
+VIDEO_RATE = 35.00 
+HST_RATE = .15 
+STAN_MONTH_RATE = 75.00 
+EXEC_MONTH_RATE = 150.00 
+MON_YEAR_RATE = 12 
+PROC_RATE = 59.99 
+CAN_RATE = .60 
+
+ 
+# Define program functions.
+ 
+ 
+ 
+# Main program starts here.
+ 
+
+   
+# Gather user inputs.
+
+ 
+ 
+# Perform required calculations.
+
+
+
+# Display results
+
+
+
+# Write the values to a data file for storage.
+ 
+ 
+ 
+# Any housekeeping duties at the end of the program.
