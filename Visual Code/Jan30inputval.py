13b284aa1515bd7f4e0be5dd39a9dad12b7f6b70
KassFequet <kassaundra.fequet@keyin.com>
2025-01-30 16:27:27 -0330

    update

---
+++ Visual Code/Jan30inputval.py
@@ -0,0 +1,46 @@
+# Description: Template
+# Author:
+# Date(s):
+
+
+# Define required libraries.
+
+
+
+# Define program constants.
+
+
+# Define program functions.
+
+
+
+# Main program starts here.
+while True:
+
+
+    # Gather user inputs.
+    while True:
+        CustName = input("Enter the customer's name: ")
+        
+        if CustName =="":
+            print()
+            print("      User Entry Error - Customer name cannot be blank" )
+            print()
+        else:
+            break
+    while True:
+        AfterHours = input("Was this job completed after hours (Y / N): "))
+
+    # Perform required calculations.
+
+
+
+    #Display results
+
+
+
+    # Write the values to a data file for storage.
+
+
+
+# Any housekeeping duties at the end of the program.
